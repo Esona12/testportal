473c42831149bd25a4720429b386944453afea07
root <root@mydns-apps.m21.gov.local>
2026-07-10 15:41:09 -0400

    Prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+       // $middleware->redirectUsersTo('/lead-up-review/dashboard');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
